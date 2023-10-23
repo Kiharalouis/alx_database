1b86e7806af4aca152f89cd50bee7757f7359cfc
Kiharalouis <kiharalouis@gmail.com>
2023-10-23 17:36:14 +0300

    ..

--- SQL_introduction/9-full_creation.sql
+++ SQL_introduction/9-full_creation.sql
@@ -1,15 +1,16 @@
---create a table
-CREATE TABLE IF NOT EXISTS third_table(
+-- Create the second_table if it doesn't exist
+CREATE TABLE IF NOT EXISTS second_table(
     id INT,
-    name VARCHAR(65),
+    name VARCHAR(256),
     score INT
 );
 
---insert into the created table
-INSERT INTO third_table (id, name, score)
+-- Insert the records into the second_table
+INSERT INTO second_table (id, name, score)
     VALUES
         (1, 'John', 10),
         (2, 'Alex', 3),
         (3, 'Bob', 14),
-        (4, 'Charlie', 20),
-        (5, 'David', 1);
+        (4, 'George', 8);
+
+        
